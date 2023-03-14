4d745b47bd9524d5122e8f653d66fd02e3e5b702
Elya Fileva <filevaelya@mail.ru>
2023-03-14 16:44:18 +0300

    Добавлена задача 9.1

---
+++ 9.1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите число М: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Четные числа от {m} до {n} --> ");
+GetEvenNumbersFromMtoN(m, n);
+void GetEvenNumbersFromMtoN(int min, int max)
+{
+  if (min < 0 || max < 0 || max < min) Console.Write($"Условие не корректно ");
+  else
+  {
+    if (min % 2 != 0) min++;
+    Console.Write("{0} ", min);
+    if (min == max || min == max + 1) return;
+    GetEvenNumbersFromMtoN(min + 2, max);
+  }
+}
